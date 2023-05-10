03300c492020a361e9f6b6a76a86790f184db3ef
unknown <alejandrovt1996@gmail.com>
2023-05-10 12:34:30 +0200

    databases

--- client/index.js
+++ client/index.js
@@ -1,14 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'alejandrovt',
+    user: 'postgres',
     host: 'localhost',
-    database: 'users',
+    database: 'database',
     password: '1234',
     port: 5432, 
 });
 
-pool.query('SELECT NOW()', (err, res) => {
+pool.query('SELECT * FROM USERS', (err, res) => {
     console.log(err, res);
     pool.end();
 });
---
+++ client/src/app/login/login.components.js
@@ -0,0 +1,22 @@
+// const form = document.querySelector('form');
+// const usernameInput = document.querySelector('#username');
+// const passwordInput = document.querySelector('#password');
+// const submitButton = document.querySelector('button[type="submit"]');
+
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault(); 
+
+//     submitButton.innerHTML = '<i class="fas fa-spinner fa-spin"></i> Loading...';
+
+//     setTimeout(() => {
+//     const usernameValue = usernameInput.value.trim();
+//     const passwordValue = passwordInput.value.trim();
+//     if (usernameValue === '' || passwordValue === '') {
+//         alert('Please enter both a username and password.');
+//         submitButton.innerHTML = 'Log in'; 
+//         return;
+//     }
+
+//     form.submit();
+//     }, 2000); 
+// });
